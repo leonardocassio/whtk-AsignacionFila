8b61467fc26455f84991bce8b561847d3b7bba47
Jáder Oliveiraa <32147441+jaderoliveiraa@users.noreply.github.com>
2023-01-10 08:43:53 -0300

    Update 20200904070004-create-default-users.ts

--- backend/src/database/seeds/20200904070004-create-default-users.ts
+++ backend/src/database/seeds/20200904070004-create-default-users.ts
@@ -7,7 +7,7 @@ module.exports = {
       [
         {
           name: "Administrador",
-          email: "admin@infotechplus.com.br",
+          email: "admin@infotechpluscariri.com.br",
           passwordHash: "$2a$08$WaEmpmFDD/XkDqorkpQ42eUZozOqRCPkPcTkmHHMyuTGUOkI8dHsq",
           profile: "admin",
           tokenVersion: 0,
